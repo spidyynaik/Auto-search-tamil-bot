b25beed724717fa35f1423d28c81b8ecb30e1b6d
Mr.Spidy <128451864+spidyynaik@users.noreply.github.com>
2023-07-17 17:47:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Autobot
 fi
-cd /Autobot
+cd /Autoobot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
